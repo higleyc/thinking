e5e5a07d4d87a13bd763bc307902d37b464765ff
Chris Higley <higleyc@gmail.com>
2016-04-28 11:08:43 -0400

    Added tag autogeneration

--- thinking-web/public/javascripts/thinking.js
+++ thinking-web/public/javascripts/thinking.js
@@ -34,8 +34,12 @@ function addThought(text, tags, callback) {
 function stageThought() {
     $('#thoughtInput').prop('disabled', true);
     
-    //TODO: run NLP
     var text = $('#thoughtInput').val();
+    //FIXME: some unnecessary operations here
+    var tags = generateTags(text);
+    tags.forEach(function(tag) {
+        stageTag(tag);
+    })
     
     $('#stagedText').text(text);
     $('#stage').css('visibility', 'visible');
@@ -71,4 +75,22 @@ function thoughtAdded() {
     $('#thoughtInput').val('');
     $('#thoughtInput').prop('disabled', false);
     $('#thoughtInput').focus();
+}
+
+function generateTags(text) {
+    // For now, pull out singularized nouns
+    // Exclude pronouns
+    var terms = nlp_compromise.text(text).terms();
+    var tags = [];
+    for (var i = 0; i < terms.length; i++) {
+        if (terms[i].pos.Noun && (!terms[i].pos.Pronoun)) {
+            if (terms[i].pos.Plural) {
+                tags.push(terms[i].singularize());
+            } else {
+                tags.push(terms[i].text);
+            }
+        }
+    }
+    
+    return tags;
 }
