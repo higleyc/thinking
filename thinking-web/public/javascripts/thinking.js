5875ebd9ec55103d0b133db56672d8c158dbaf55
Chris Higley <higleyc@gmail.com>
2016-04-28 10:38:46 -0400

    Added thought staging, basic tags

--- thinking-web/public/javascripts/thinking.js
+++ thinking-web/public/javascripts/thinking.js
@@ -1,20 +1,74 @@
+stagedTags = [];
+
 $(function(){
+    $('#thoughtInput').focus();
+    
     $('#thoughtBox').submit(function(event) {
         event.preventDefault();
         
-        addThought($('#thoughtInput').val(), function() {
-            $('#thoughtInput').val('');
-        });
+//        addThought($('#thoughtInput').val(), function() {
+//            $('#thoughtInput').val('');
+//        });
+        stageThought();
+    });
+    
+    $('#tagForm').submit(function(event) {
+        event.preventDefault();
+        
+        tagAction();
     });
 });
 
-function addThought(text, callback) {
+function addThought(text, tags, callback) {
     $.post(
         '/api/thoughts',
         {
-            text: text
+            text: text,
+            tags: tags
         }
     ).always(function() { 
         callback();
     });
+}
+
+function stageThought() {
+    $('#thoughtInput').prop('disabled', true);
+    
+    //TODO: run NLP
+    var text = $('#thoughtInput').val();
+    
+    $('#stagedText').text(text);
+    $('#stage').css('visibility', 'visible');
+    $('#tagInput').focus();
+}
+
+function tagAction() {
+    var tagText = $('#tagInput').val();
+    
+    if (tagText == '') {
+        addThought($('#stagedText').text(), stagedTags, thoughtAdded);
+        unstage();
+    } else {
+        $('#tagInput').val('');
+        stageTag(tagText);
+    }
+}
+
+function stageTag(tagText) {
+    stagedTags.push(tagText);
+    
+    $('#stagedTags').append('<span class="tag">' + tagText + '</span>');
+}
+
+function unstage() {
+    $('#stage').css('visibility', 'hidden');
+    stagedTags = [];
+    $('#stagedText').text('');
+    $('#stagedTags').text('');
+}
+
+function thoughtAdded() {
+    $('#thoughtInput').val('');
+    $('#thoughtInput').prop('disabled', false);
+    $('#thoughtInput').focus();
 }
