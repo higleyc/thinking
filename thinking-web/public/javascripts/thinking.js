a29a277d5aa35be60aecfb1ee6a00ff7147d3f03
Chris Higley <higleyc@gmail.com>
2016-04-28 13:30:32 -0400

    All tags to lower case

--- thinking-web/public/javascripts/thinking.js
+++ thinking-web/public/javascripts/thinking.js
@@ -88,9 +88,9 @@ function generateTags(text) {
     for (var i = 0; i < terms.length; i++) {
         if (terms[i].pos.Noun && (!terms[i].pos.Pronoun)) {
             if (terms[i].pos.Plural) {
-                tags.push(terms[i].singularize());
+                tags.push(terms[i].singularize().toLowerCase());
             } else {
-                tags.push(terms[i].text);
+                tags.push(terms[i].text.toLowerCase());
             }
         }
     }
